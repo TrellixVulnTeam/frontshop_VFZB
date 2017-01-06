e9503e6e3f6c2eb3647497a5e7e12f5da4e9f4eb
--replace-all <fnduister@gmail.com>
2017-01-05 22:45:05 -0500

    gitignore active

--- src/Containers/Header.js
+++ src/Containers/Header.js
@@ -1,4 +1,6 @@
 import React, {Component} from 'react';
+import {Grid,Row,Col} from 'react-bootstrap';
+import '../css/App.css'
 
 export default class Header extends Component {
     constructor() {
@@ -7,7 +9,13 @@ export default class Header extends Component {
 
     render() {
         return (
-            <p>this is the header</p>
+            <Grid fluid={true}>
+                <Row className="show-grid">
+                    <Col sm={3}>title</Col>
+                    <Col sm={6} bsClass="backmenu col">menu</Col>
+                    <Col sm={3}>email</Col>
+                </Row>
+            </Grid>
         );
     }
 }
